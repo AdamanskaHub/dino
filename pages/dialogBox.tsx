7bdc66fe9d05eff3c6a0de9bf5e60356390dfab3
Emilie <adamanska@gmail.com>
2021-03-11 17:00:49 +0100

    cleaner and cookies working

--- pages/dialogBox.tsx
+++ pages/dialogBox.tsx
@@ -2,14 +2,22 @@ import React, { useState,useEffect } from "react";
 import Message from './messages';
 
 const messages = [
-    [
+    [ // Level 0
         [{pic: '/other.png', txt:"ske 0"}, {pic: '/hunk.png', txt:"frog 0"}],
         [{pic: '/other.png', txt:"ske 1"}, {pic: '/hunk.png', txt:"frog b1"}], 
         [{pic: '/other.png', txt:"ske 2"}, {pic: '/hunk.png', txt:"frog 2"}],
     ], 
-    [["lv 2 - 1", "this is lv 2 - 2"],["more lines - 1", "lots of lines - 2"]],
-
-    ];
+    [ // Level 1
+        [{pic: '/other.png', txt:"ske 1.0"}, {pic: '/hunk.png', txt:"frog 1.0"}],
+        [{pic: '/other.png', txt:"ske 1.1"}, {pic: '/hunk.png', txt:"frog b1.1"}], 
+        [{pic: '/other.png', txt:"ske 1.2"}, {pic: '/hunk.png', txt:"frog 1.2"}],
+    ], 
+    [ // Level 2
+        [{pic: '/other.png', txt:"ske 2.0"}, {pic: '/hunk.png', txt:"frog 2.0"}],
+        [{pic: '/other.png', txt:"ske 2.1"}, {pic: '/hunk.png', txt:"frog b2.1"}], 
+        [{pic: '/other.png', txt:"ske 2.2"}, {pic: '/hunk.png', txt:"frog 2.2"}],
+    ], 
+];
 
 
 let lv = 0;
@@ -17,38 +25,32 @@ const cookieValue = () => {
     if (document.cookie === undefined || '' ){
         return '0'
     } else {
-        console.log(document.cookie)
-        return document.cookie.split('; ').find(row => row.startsWith('myLv=')).split('=')[1]};
-    }
-    
+        console.log('cookie creation : '+document.cookie)
+        return document.cookie.split('; ').find(row => row.startsWith('myLv=')).split('=')[1]
+    };
+};
 
 
 export default function DialogBox() {
 
     const [currentMessage, setCurrentMessage] = useState('');
-    const [position, setPosition] = useState(0);
-    const [pic, setPic] = useState('/hunk.png')
+    const [position, setPosition] = useState(0); // position in the text, line #
+    const [pic, setPic] = useState('')
 
     const randomize = (lengthor) => { return Math.floor(Math.random() * lengthor.length)}
 
     React.useEffect(() => {
-        nextLine();
-        document.cookie = `myLv=0; path=/; secure=Lax; samesite=Lax; expires=Tue, 01 Jan 2030 00:00:00 GMT"`
+        document.cookie = `myLv=1; path=/; secure=Lax; samesite=Lax; expires=Tue, 01 Jan 2030 00:00:00 GMT"`
         parseInt(cookieValue()) !== undefined ? lv = parseInt(cookieValue()) : lv = 0;
-        
+        nextLine();
     }, [])// <-- empty array means 'run once'
 
     const nextLine = () => {
         console.log(lv)
         if (position <= messages[lv].length - 1) {
-            // console.log('in if statement '+ position + ' ' + messages[lv] + 'RANDOM = '+ randomize(messages[lv][position]))
-            // IF it's an image do something else like setting the image
             const randomNum = randomize(messages[lv][position])
-            // set the img
             setPic(messages[lv][position][randomNum].pic)
-            // set the text
             setCurrentMessage(messages[lv][position][randomNum].txt)
-            // setCurrentMessage(messages[lv][position][randomize(messages[lv][position])])
             setPosition(position+1)
         }
     };
@@ -60,11 +62,11 @@ export default function DialogBox() {
             <div onClick={nextLine}>
             Next
             </div>
-            {/* <button onClick={() => document.cookie = `myLv=0; path=/; secure=Lax; samesite=Lax; 
-            expires=Tue, 01 Jan 2030 00:00:00 GMT"`}>CREATE COOKIE 0</button>
-            <button onClick={() => document.cookie = `myLv=33;`}>CREATE COOKIE 33</button>
-            <button onClick={() => console.log(document.cookie + ' ' + lv)}>SHOW ME </button> */}
-            <img src={pic} alt="workout"/>
+            <button onClick={() => document.cookie = `myLv=1; path=/; secure=Lax; samesite=Lax; 
+            expires=Tue, 01 Jan 2030 00:00:00 GMT"`}>BRING ME AT LEVEL 1</button>
+            <button onClick={() => document.cookie = `myLv=0; path=/; secure=Lax; samesite=Lax; 
+            expires=Tue, 01 Jan 2030 00:00:00 GMT"`}>BRING ME AT LEVEL 0</button>
+            <img src={pic} alt="coach image"/>
         </div>
     )
 }
