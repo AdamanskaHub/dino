45328b68c3a86f7151f30843331ece22ebf7099b
Emilie <adamanska@gmail.com>
2021-03-16 14:47:27 +0100

    Prettier config not fun

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,95 @@
-import Head from 'next/head'
-import styles from '../styles/globals.css'
-import { useState } from 'react'
-import Countdown from './countdown'
-import DialogBox from './dialogBox'
+import Head from "next/head";
+import styles from "../styles/globals.css";
+import { useState } from "react";
+import Countdown from "./countdown";
+import DialogBox from "./dialogBox";
 
 export default function Home() {
+	const [activeView, setActiveView] = useState(false);
+	const changeView = () => setActiveView(!activeView);
 
-  const [activeView, setActiveView] = useState(false);
-  const changeView = () => setActiveView(!activeView)
+	const [duration, setDuration] = useState(0);
+	const [timeOver, setTimeOver] = useState(false);
+	const [activity, setActivity] = useState("welcome");
 
-  const [duration, setDuration] = useState(0)
-  const [timeOver, setTimeOver] = useState(false)
-  const [activity, setActivity] = useState("welcome")
+	return (
+		<div className="bg">
+			{/* <Head>
+<title>Motivator</title>
+<link rel="icon" href="/favicon.ico" />
+</Head> */}
+			<main>
+				{!activeView ? (
+					<>
+						<DialogBox screen={activity} />
+						<div className="action-box">
+							<div className="action-box action-box__title"></div>
+							<div className="action-box action-box__buttons">
+								<div
+									className="action-btn"
+									onClick={() => setActivity("study")}
+								>
+									Study
+								</div>
+								<div
+									className="action-btn"
+									onClick={() => setActivity("workout")}
+								>
+									Workout
+								</div>
+								<div
+									className="action-btn"
+									onClick={() => setActivity("other")}
+								>
+									Other
+								</div>
+							</div>
+						</div>
+						{/* <p>
+How long for?
+<input onChange={event => setDuration(parseInt(event.target.value))}></input>
+</p> */}
+						<p>What are you working on?</p>
+						<p>{activity}</p>
 
-  return (
-    
-    <div className='bg'>
-      {/* <Head>
-        <title>Motivator</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head> */}
-      <main>
-        {!activeView ? (
-          <>
-            <DialogBox screen={activity}/>
-            <h1 >
-              Let's do this
-        </h1>
-            {/* <p>
-              How long for?
-          <input onChange={event => setDuration(parseInt(event.target.value))}></input>
-            </p> */}
-            <p >
-              What are you working on?
-            </p>
-            <p>{activity}</p>
-            <div onClick={()=>setActivity("study")}>Study</div>
-            <div onClick={()=>setActivity("workout")}>Workout</div>
-            <div onClick={()=>setActivity("other")}>Other</div>
+						<p>How long for?</p>
+						{/* <button  onClick={()=>{setDuration(12); changeView()}}>12min</button>
+<button  onClick={()=>{setDuration(5); changeView()}}>5min</button> */}
 
-            <p>How long for?</p>
-            {/* <button  onClick={()=>{setDuration(12); changeView()}}>12min</button>
-            <button  onClick={()=>{setDuration(5); changeView()}}>5min</button> */}
+						<button onClick={() => setDuration(12)}>12min</button>
+						<button onClick={() => setDuration(5)}>5min</button>
 
-            <button  onClick={()=>setDuration(12)}>12min</button>
-            <button  onClick={()=>setDuration(5)}>5min</button>
-
-            {activity != '' && duration != 0 && 
-              <button onClick={changeView}>Let's {activity} for {duration}min</button>
-            }
-          </>
-        ) : (
-            <>
-              <p>active view</p>
-              <DialogBox screen={activity}/>
-              <Countdown duration={duration} countdownFinished={timeOver =>{ 
-                (setTimeOver(true)),
-                // (changeView()),
-                (console.log("FINISHED"))
-                }}
-              />
-              <button onClick={changeView}>End</button>
-            </>
-          )}
-          {timeOver &&
-          <p>countdown finished</p>
-          }
-
-      </main>
-    </div>
-  )
+						{activity != "" && duration != 0 && (
+							<button onClick={changeView}>
+								Let's {activity} for {duration}min
+							</button>
+						)}
+					</>
+				) : (
+					<>
+						<p>active view</p>
+						<DialogBox screen={activity} />
+						<Countdown
+							duration={duration}
+							countdownFinished={(timeOver) => {
+								setTimeOver(true),
+									// (changeView()),
+									console.log("FINISHED");
+							}}
+						/>
+						<button onClick={changeView}>End</button>
+					</>
+				)}
+				{timeOver && <p>countdown finished</p>}
+			</main>
+		</div>
+	);
 }
-
 
 // TODO
 // ---  Penser que c'est comme l'autre jeu débile ---
 // Intro dialogue
-// Choisir ce que je veux faire 
-// lancer le timer 
-// Plus de dialogue 
-// Autre choix 
+// Choisir ce que je veux faire
+// lancer le timer
+// Plus de dialogue
+// Autre choix
---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  // "semi": true,
+  // "trailingComma": "none",
+  // "singleQuote": true,
+  // // "printWidth": 80,
+  "tabWidth": 4,
+  tabWidth: 4,
+  // "singleQuote": true,
+  "useTabs": true,
+  "editor.tabSize": 2,
+
+
+};
